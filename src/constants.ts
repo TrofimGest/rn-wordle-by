f1c355ebaaa43393aa962b640bc66963d6011f4b
TrofimGest <gubaoslika@gmail.com>
2023-03-07 15:24:02 +0300

    feat: add Keyboard and Map

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  black: '#121214',
+  darkgrey: '#3A3A3D',
+  grey: '#818384',
+  lightgrey: '#D7DADC',
+  primary: '#538D4E',
+  secondary: '#B59F3B',
+};
+
+export const colorsToEmoji = {
+  [colors.darkgrey]: '⬛',
+  [colors.primary]: '🟩',
+  [colors.secondary]: '🟧',
+};
+
+export const ENTER = 'УВОД';
+export const CLEAR = '⌫';
+
+export const keys = [
+  ['ё', 'й', 'ц', 'у', 'к', 'е', 'г', 'ш', 'ў', 'з', 'х', "'"],
+  ['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э'],
+  [ENTER, 'я', 'ч', 'с', 'м', 'і', 'т', 'ь', 'б', 'ю', CLEAR],
+];
