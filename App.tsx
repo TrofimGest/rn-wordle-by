0a91d68112430c5d08870f71bf7e6b8a2c1fae88
TrofimGest <gubaoslika@gmail.com>
2023-03-08 15:14:15 +0300

    refactor: refactor code

--- App.tsx
+++ App.tsx
@@ -1,162 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-} from 'react-native';
-import Clipboard from '@react-native-clipboard/clipboard';
-import {colors, CLEAR, ENTER, colorsToEmoji} from './src/constants';
-import Keyboard from './src/components/Keyboard';
-
-const NUMBER_OF_ROWS = 6;
-
-const copyArray = arr => {
-  return [...arr.map(rows => [...rows])];
-};
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import Game from './src/components/Game';
+import {colors} from './src/constants';
 
 function App(): JSX.Element {
-  const word = 'лазер';
-  const letters = word.split('');
-  const [rows, setRows] = useState(
-    new Array(NUMBER_OF_ROWS).fill(new Array(letters.length).fill('')),
-  );
-  const [currentRow, setCurrentRow] = useState(0);
-  const [currentColumn, setCurrentColumn] = useState(0);
-  const [gameState, setGameState] = useState('playing'); // won, lost, playing
-
-  useEffect(() => {
-    if (currentRow > 0) {
-      checkGameState();
-    }
-  }, [currentRow]);
-
-  const checkGameState = () => {
-    if (checkIfWon() && gameState !== 'won') {
-      Alert.alert('Huraaay', 'You won!', [
-        {text: 'Share', onPress: shareScore},
-      ]);
-      setGameState('won');
-    } else if (checkIfLost() && gameState !== 'lost') {
-      Alert.alert('Meh', 'Try again tomorrow!');
-      setGameState('lost');
-    }
-  };
-
-  const checkIfWon = () => {
-    const row = rows[currentRow - 1];
-    return row.every((letter, i) => letter === letters[i]);
-  };
-
-  const checkIfLost = () => {
-    return !checkIfWon() && currentRow === rows.length;
-  };
-
-  const shareScore = () => {
-    const textMap = rows
-      .map((row, i) =>
-        row.map(j => colorsToEmoji[getCellBGColor(i, j)]).join(''),
-      )
-      .filter(row => row)
-      .join('\n');
-    const textToShare = `Wordle \n${textMap}`;
-    Clipboard.setString(textToShare);
-    Alert.alert('Copied successfully', 'Share your score on you social media');
-  };
-
-  const onKeyPressed = (key: string[]) => {
-    if (gameState !== 'playing') {
-      return;
-    }
-    const updatedRows = copyArray(rows);
-
-    if (key === CLEAR) {
-      const previousColumn = currentColumn - 1;
-      if (previousColumn >= 0) {
-        updatedRows[currentRow][previousColumn] = '';
-        setRows(updatedRows);
-        setCurrentColumn(previousColumn);
-      }
-      return;
-    }
-
-    if (key === ENTER) {
-      if (currentColumn === rows[0].length) {
-        setCurrentRow(currentRow + 1);
-        setCurrentColumn(0);
-      }
-      return;
-    }
-
-    if (currentColumn < rows[0].length) {
-      updatedRows[currentRow][currentColumn] = key;
-      setRows(updatedRows);
-      setCurrentColumn(currentColumn + 1);
-    }
-  };
-
-  const isCellActive = (row, column) => {
-    return row === currentRow && column === currentColumn;
-  };
-
-  const getCellBGColor = (row, column) => {
-    const letter = rows[row][column];
-
-    if (row >= currentRow) {
-      return colors.black;
-    }
-    if (letter === letters[column]) {
-      return colors.primary;
-    }
-    if (letters.includes(letter)) {
-      return colors.secondary;
-    }
-    return colors.darkgrey;
-  };
-
-  const getAllLettersWithColor = color => {
-    return rows.flatMap((row, i) =>
-      row.filter((cell, j) => getCellBGColor(i, j) === color),
-    );
-  };
-
-  const greenCaps = getAllLettersWithColor(colors.primary);
-  const yellowCaps = getAllLettersWithColor(colors.secondary);
-  const greyCaps = getAllLettersWithColor(colors.darkgrey);
-
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" />
       <Text style={styles.title}>ЎОРДЛІ</Text>
-      <View style={styles.map}>
-        {rows.map((row, i) => (
-          <View key={`row:${i}`} style={styles.row}>
-            {row.map((letter: string, j: number) => (
-              <View
-                key={`cell:${i}:${j}`}
-                style={[
-                  styles.cell,
-                  {
-                    borderColor: isCellActive(i, j)
-                      ? colors.grey
-                      : colors.darkgrey,
-                    backgroundColor: getCellBGColor(i, j),
-                  },
-                ]}>
-                <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
-              </View>
-            ))}
-          </View>
-        ))}
-      </View>
-      <Keyboard
-        onKeyPressed={onKeyPressed}
-        greenCaps={greenCaps}
-        yellowCaps={yellowCaps}
-        greyCaps={greyCaps}
-      />
+      <Game />
     </SafeAreaView>
   );
 }
@@ -172,30 +24,6 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: 'bold',
     letterSpacing: 7,
-  },
-  map: {
-    alignSelf: 'stretch',
-    marginVertical: 20,
-  },
-  row: {
-    alignSelf: 'stretch',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  cell: {
-    borderWidth: 3,
-    borderColor: colors.darkgrey,
-    flex: 1,
-    maxWidth: 70,
-    aspectRatio: 1,
-    margin: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  cellText: {
-    color: colors.lightgrey,
-    fontWeight: 'bold',
-    fontSize: 28,
   },
 });
 
---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,167 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View, Alert} from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+import {colors, CLEAR, ENTER, colorsToEmoji} from '../../constants';
+import styles from './Game.styles';
+import Keyboard from '../../components/Keyboard';
+import dictionary from '../../dictionary';
+
+const NUMBER_OF_ROWS = 6;
+
+const getDayOfTheYear = () => {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = now - start;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const day = Math.floor(diff / oneDay);
+  return day;
+};
+const dayOfTheYear = getDayOfTheYear();
+
+const copyArray = arr => {
+  return [...arr.map(rows => [...rows])];
+};
+
+const Game = (): JSX.Element => {
+  const word = dictionary[dayOfTheYear];
+  const letters = word.split('');
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_ROWS).fill(new Array(letters.length).fill('')),
+  );
+  const [currentRow, setCurrentRow] = useState(0);
+  const [currentColumn, setCurrentColumn] = useState(0);
+  const [gameState, setGameState] = useState('playing'); // won, lost, playing
+
+  useEffect(() => {
+    if (currentRow > 0) {
+      checkGameState();
+    }
+  }, [currentRow]);
+
+  const checkGameState = () => {
+    if (checkIfWon() && gameState !== 'won') {
+      Alert.alert('Huraaay', 'You won!', [
+        {text: 'Share', onPress: shareScore},
+      ]);
+      setGameState('won');
+    } else if (checkIfLost() && gameState !== 'lost') {
+      Alert.alert('Meh', 'Try again tomorrow!');
+      setGameState('lost');
+    }
+  };
+
+  const checkIfWon = () => {
+    const row = rows[currentRow - 1];
+    return row.every((letter, i) => letter === letters[i]);
+  };
+
+  const checkIfLost = () => {
+    return !checkIfWon() && currentRow === rows.length;
+  };
+
+  const shareScore = () => {
+    const textMap = rows
+      .map((row, i) =>
+        row.map(j => colorsToEmoji[getCellBGColor(i, j)]).join(''),
+      )
+      .filter(row => row)
+      .join('\n');
+    const textToShare = `Wordle \n${textMap}`;
+    Clipboard.setString(textToShare);
+    Alert.alert('Copied successfully', 'Share your score on you social media');
+  };
+
+  const onKeyPressed = (key: string[]) => {
+    if (gameState !== 'playing') {
+      return;
+    }
+    const updatedRows = copyArray(rows);
+
+    if (key === CLEAR) {
+      const previousColumn = currentColumn - 1;
+      if (previousColumn >= 0) {
+        updatedRows[currentRow][previousColumn] = '';
+        setRows(updatedRows);
+        setCurrentColumn(previousColumn);
+      }
+      return;
+    }
+
+    if (key === ENTER) {
+      if (currentColumn === rows[0].length) {
+        setCurrentRow(currentRow + 1);
+        setCurrentColumn(0);
+      }
+      return;
+    }
+
+    if (currentColumn < rows[0].length) {
+      updatedRows[currentRow][currentColumn] = key;
+      setRows(updatedRows);
+      setCurrentColumn(currentColumn + 1);
+    }
+  };
+
+  const isCellActive = (row, column) => {
+    return row === currentRow && column === currentColumn;
+  };
+
+  const getCellBGColor = (row, column) => {
+    const letter = rows[row][column];
+
+    if (row >= currentRow) {
+      return colors.black;
+    }
+    if (letter === letters[column]) {
+      return colors.primary;
+    }
+    if (letters.includes(letter)) {
+      return colors.secondary;
+    }
+    return colors.darkgrey;
+  };
+
+  const getAllLettersWithColor = color => {
+    return rows.flatMap((row, i) =>
+      row.filter((cell, j) => getCellBGColor(i, j) === color),
+    );
+  };
+
+  const greenCaps = getAllLettersWithColor(colors.primary);
+  const yellowCaps = getAllLettersWithColor(colors.secondary);
+  const greyCaps = getAllLettersWithColor(colors.darkgrey);
+
+  return (
+    <>
+      <View style={styles.map}>
+        {rows.map((row, i) => (
+          <View key={`row:${i}`} style={styles.row}>
+            {row.map((letter: string, j: number) => (
+              <View
+                key={`cell:${i}:${j}`}
+                style={[
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(i, j)
+                      ? colors.grey
+                      : colors.darkgrey,
+                    backgroundColor: getCellBGColor(i, j),
+                  },
+                ]}>
+                <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+      <Keyboard
+        onKeyPressed={onKeyPressed}
+        greenCaps={greenCaps}
+        yellowCaps={yellowCaps}
+        greyCaps={greyCaps}
+      />
+    </>
+  );
+};
+
+export default Game;
